b425d1fe9814128acc4b31d3aa39f63051f179b0
Tariq Brown <132512009+tariqsb@users.noreply.github.com>
2023-11-08 20:29:09 -0800

    Create Lobby page

--- src/pages/CreateLobby/BotSettings.tsx
+++ src/pages/CreateLobby/BotSettings.tsx
@@ -12,28 +12,31 @@ export function BotSettings() {
 
   return (
     <div style={{ 
-      backgroundColor: '#535353',
+      backgroundColor: '#F9F8F7',
       borderRadius: 20,
       justifyContent: 'center',
       alignItems: 'center',
       height: 468,
       width: 366,
-      color: '#E3E3E3'
+      color: '#535353',
+      boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+      paddingLeft: 10,
+      paddingRight: 10
       }}>
 
-      <p style={{ fontSize: 25, marginBottom: 20, marginTop: 20, textAlign: 'center'}}>Bot Settings</p>
+      <p style={{ fontSize: 25, marginBottom: 20, marginTop: 20, textAlign: 'center', color: '#383838'}}>Bot Settings</p>
       <hr style={{ borderTop: '2px solid #E3E3E3', borderLeft: 'none', width: 366 }} />
 
       <div style={{ margin: 20 }}>
         <p style={{ fontSize: 22, paddingBottom: 10}}>{'Custom Name'}</p>
 
-        <input style={{ backgroundColor: '#F9F8F7', borderRadius: 20, width: 306, height: 34, border: 0, outline: 0, paddingLeft: 10 }}></input>
+        <input style={{ backgroundColor: '#F9F8F7', borderRadius: 20, width: 306, height: 34, paddingLeft: 10, border: '1px solid #C4C4C4', outline: 0}}></input>
       </div>
 
       <div style={{margin: 20}}>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <p style={{ fontSize: 22, marginTop: 0, marginBottom: 15 }}>Assertiveness</p>
-            <div style={{ borderRadius: 50, height: 22, width: 22, backgroundColor: '#F9F8F7', textAlign: 'center', color: '#6E553D', marginRight: 10}}><p>?</p></div>
+            <div style={{ borderRadius: 50, height: 22, width: 22, backgroundColor: '#F9F8F7', textAlign: 'center', color: '#6E553D', marginRight: 10, border: '1px solid #C4C4C4', outline: 0}}><p>?</p></div>
         </div>
         <Slider
             aria-label="Assertiveness"
