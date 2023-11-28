f291de58d4c0bf0a8b7cd5281d05a7d230f1eee3
Tariq Brown <132512009+tariqsb@users.noreply.github.com>
2023-11-27 21:05:27 -0800

    added different states to Join Lobby Page

--- src/pages/JoinLobby/JoinLobby.tsx
+++ src/pages/JoinLobby/JoinLobby.tsx
@@ -1,24 +1,15 @@
 import './JoinLobby.css';
 import React, { useState } from 'react';
 
+interface LobbbyInformationProps {
+  users : string[];
+}
+
 export function JoinLobby() {
-  const [boxes, setBoxes] : any[] = useState([]);
   const [name, setName] = useState('Guest');
-
-  const addBox = () => {
-    // Create a new box element (a div) with the desired content
-    const newBox = (
-      <div className="b" key={boxes.length}>
-        <div className="profile-icon">
-          <img src="logo.jpg" alt="Logo" className="logo-icon" />
-        </div>
-        <div className="box-content">New Box {boxes.length + 1}</div>
-      </div>
-    );
-
-    // Update the state to include the new box
-    setBoxes([...boxes, newBox]);
-  };
+  const [code, setCode] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  const [lobbyState, setLobbyState] = useState('Not Joined');
 
   // Initialize the state with x boxes when the component is mounted
   React.useEffect(() => {
@@ -30,30 +21,26 @@ export function JoinLobby() {
 
     // Set the name synchronously before initializing the boxes
     setName(() => formattedName);
-
-    const initialBoxes = [];
-    for (let i = 0; i < 2; i++) {
-      let boxContent;
-      if (i === 0) {
-        boxContent = 'Host';
-      } else if (i === 1) {
-        boxContent = `${formattedName}`;
-      } else {
-        boxContent = `New Box ${i + 1}`;
-      }
-
-      const newBox = (
-        <div className="b" key={i}>
-          <div className="profile-icon">
-            <img src="logo.jpg" alt="Logo" className="logo-icon" />{' '}
-          </div>
-          <div className="box-content">{boxContent}</div>
-        </div>
-      );
-      initialBoxes.push(newBox);
-    }
-    setBoxes(initialBoxes);
   }, []);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setLobbyState('Waiting');
+
+    // call api to JOIN LOBBY, RETURN CALL WILL INCLUDE LOBBY INFO AND LIST OF USERS INCLUDING THIS ONE
+
+    // CHECK IF LOBBY EXISTS
+    if (code === 'asdf') {
+      setLobbyState('Joined');
+      setDisabled(true);
+      // SET LOBBY INFORMATION
+    } else {
+      setLobbyState('Error');
+    }
+
+    //TODO: ADD STATEMENT IF LOBBY CODE DOESNT EXIST AND IF API CALL RETURNS AN ERROR
+  };
 
   return (
     //three main sections: screen, content box, members box
@@ -72,12 +59,67 @@ export function JoinLobby() {
       <div className="content-box">
         <h2>Enter Code</h2>
         <div className="joinbox">
-          <input className="joininput" type="text" placeholder="" />
+          <form onSubmit={handleSubmit}>
+            <input 
+              className="joininput" 
+              type="text" placeholder="" 
+              value={code} 
+              onSubmit={handleSubmit} 
+              disabled={disabled} 
+              onChange={(e) => {
+                if (e.target.value.length <= 4) {
+                  setCode(e.target.value);
+                }}} />
+          </form>
         </div>
-
-        <p className="waiting-paragraph">Waiting for host . . .</p>
       </div>
 
+      {(lobbyState === 'Waiting') && <p className="waiting-paragraph">Attempting to join lobby...</p>}
+      {(lobbyState === 'Joined') && <LobbyInformation users={['Tariq', 'Kai', name]}/>}
+      {(lobbyState === 'Error') && <p className="waiting-paragraph">Error joining room. Please try again.</p>}
+      
+    </div>
+  );
+}
+
+interface UserBoxProps {
+  content: string;
+  index : number;
+}
+
+function LobbyInformation(props : LobbbyInformationProps) {
+  const [boxes, setBoxes] : any[] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  React.useEffect(() => {
+    let initialBoxes = [];
+    for (let i = 0; i < props.users.length; i++) {
+      initialBoxes.push(<UserBox content={props.users[i]} index={i}/>);
+    }
+
+    setBoxes(initialBoxes);
+    setIndex(props.users.length-1);
+  }, [props.users]);
+
+  // NEED TO MAKE UPDATE WHEN NEW USERS JOIN
+
+
+  const UserBox = (props : UserBoxProps) => {
+    return (
+      <div className="b" key={props.index}>
+        <div className="profile-icon">
+          <img src="logo.jpg" alt="Logo" className="logo-icon" />{' '}
+        </div>
+        <div className="box-content">{props.content}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div className='lobby-info'>
+
+      <p className="waiting-paragraph">Waiting for host . . .</p>
+      
       <div className="border-container">
         <p className="members-paragraph">Members</p>
         <div className="boxes">{boxes}</div>
@@ -85,4 +127,3 @@ export function JoinLobby() {
     </div>
   );
 }
-
